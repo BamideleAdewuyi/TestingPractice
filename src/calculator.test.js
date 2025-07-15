d1d3d4dfdb1841460e37ca6d69bb7823f0f697cb
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-15 23:37:50 +0100

    calculator.test.js: Wrote test that checks subtract method returns something

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,14 +1,18 @@
 import Calculator from "./calculator";
-const calc = new Calculator
+const calc = new Calculator;
 
 test('Add method returns something', () => {
     expect(calc.add(1, 2)).toBeDefined();
-})
+});
 
 test('Add method returns sum of two arguments', () => {
     expect(calc.add(1, 2)).toBe(3);
-})
+});
 
 test('Add method returns sum of a different two arguments', () => {
     expect(calc.add(3343, 4535)).toBe(7878)
+});
+
+test('Subtract method returns something', () => {
+    expect(calc.subtract(2,3)).toBeDefined();
 })
