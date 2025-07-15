d762963b1bcc0429e30154ffcd7f80068a337101
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-15 22:37:40 +0100

    reverseString.test.js: Wrote second test that tests function can reverse the word  "reverse"

--- src/reverseString.test.js
+++ src/reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require('./reverseString')
 test('Function returns something', () => {
     expect(reverseString("anything")).toBeDefined();
 })
+
+test('Function reverses a string', () => {
+    expect(reverseString("reverse")).toBe("esrever");
+})
