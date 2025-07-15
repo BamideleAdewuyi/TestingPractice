6e8a7619002f51b84ba703303c21cd70fc262f41
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-15 23:29:25 +0100

    calculator.test.js: Wrote second test that checks add method correctly adds 1 and 2

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,12 @@
 import Calculator from "./calculator";
 
 
-test('Object add method returns something', () => {
+test('Add method returns something', () => {
     const calc = new Calculator;
     expect(calc.add(1, 2)).toBeDefined();
+})
+
+test('Add method returns sum of two arguments', () => {
+    const calc = new Calculator;
+    expect(calc.add(1, 2)).toBe(3);
 })
