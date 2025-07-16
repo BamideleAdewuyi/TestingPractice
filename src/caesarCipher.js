813720c9cb029606ecd72c37d87b2e8be2aabbae
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-16 23:33:14 +0100

    caesarCipher.js: Function can return def from abc, 3. Passes first test

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,3 +1,8 @@
 export function caesarCipher(string, shift) {
-    return "Anything";
+    let code = [];
+    for (let i = 0; i < string.length; i++) {
+        code.push(i+97+shift)
+    }
+    let cipher = code.map((x) => String.fromCharCode(x)).join("")
+    return cipher;
 };
