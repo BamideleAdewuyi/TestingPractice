eecbae8ecd7b4a42992e258b706c0102fc55d52b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-16 23:42:14 +0100

    caesarCipher.test.js: Wrote test that checks function wraps from z to a

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -6,4 +6,8 @@ test('Function returns something', () => {
 
 test('Function shifts abc to def', () => {
     expect(caesarCipher('abc', 3)).toBe('def');
-})
+});
+
+test('Function can handle wrapping from z to a. ("xyz", 3) should return "abc"', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc')
+});
