162e6042ea6f384ac39edbfeeab96c48b996d53a
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-16 17:37:35 +0100

    caesarCipher.test.js: Wrote test that checks function shifts abc to def

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -3,3 +3,7 @@ import { caesarCipher } from "./caesarCipher";
 test('Function returns something', () => {
     expect(caesarCipher('abc', 3)).toBeDefined();
 });
+
+test('Function shifts abc to def', () => {
+    expect(caesarCipher('abc', 3)).toBe('def');
+})
