8180dd1738b22a4aca8ae2e25d1df0995f9c736d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-18 00:07:00 +0100

    caesarCipher.js: Removed | from regex

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,6 +1,6 @@
 export function caesarCipher(string, shift) {
     let code = [];
-    const letters = new RegExp(/[a-z|A-Z]/, "i")
+    const letters = new RegExp(/[a-zA-Z]/, "i")
     for (let i = 0; i < string.length; i++) {
         const newCharCode = letters.test(string[i]) ? string.charCodeAt(i) + shift : string.charCodeAt(i);
         newCharCode < 122 ? code.push(newCharCode) : code.push(newCharCode - 26);
