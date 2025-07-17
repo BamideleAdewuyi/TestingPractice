ee2e2a099dda2376364861ae3c33318756980e26
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-17 17:46:09 +0100

    caesarCipher.js: Function uses charCodeAt to get new character code. Checks if new code is out of range and subtracts 26 if so. Can now handle wrapping round z to a. Passes tests

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,7 +1,8 @@
 export function caesarCipher(string, shift) {
     let code = [];
     for (let i = 0; i < string.length; i++) {
-        code.push(i+97+shift)
+        let newCharCode = string.charCodeAt(i) + shift
+        newCharCode < 122 ? code.push(newCharCode) : code.push(newCharCode - 26)
     }
     let cipher = code.map((x) => String.fromCharCode(x)).join("")
     return cipher;
