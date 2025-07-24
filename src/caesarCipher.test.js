eea7bcf0de52c29e8bf99bf8e949e35c3668882d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-24 15:00:38 +0100

    caesarCipher.test.js: Wrote test for shifting bcd negative 1 positions. Passed

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -18,8 +18,12 @@ test('Function handles case preservation', () => {
 
 test('Function leaves punctuation unchanged', () => {
     expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
-})
+});
 
 test('Khoor, Zruog! returns Nkrru, Cuxrj!', () => {
     expect(caesarCipher("Khoor, Zruog!", 3)).toBe("Nkrru, Cuxrj!")
-})
+});
+
+test('bcd shifted -1 should be abc', () => {
+    expect(caesarCipher("bcd", -1)).toBe("abc")
+});
