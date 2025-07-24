144e99e6996443de34e5446a6a8a0e51d6ce513d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-24 14:56:05 +0100

    caesarCipher.js: Deleted wrap function. Used two regular expressions to achieve wrapping. Passes all tests.

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,18 +1,28 @@
 function charToInt(char) {
     return char.charCodeAt(0)
-}
+};
 
 function intToChar(code) {
     return String.fromCharCode(code);
-}
+};
+
+
 
 export function caesarCipher(str, shift) {
     const oldArr = str.split("")
-    const regex = /[A-Za-z]/;
-    const newCodes = oldArr.map(char => 
-    regex.test(char) ? charToInt(char) + shift : charToInt(char)
-    )
+    const upperRegex = /[A-Z]/;
+    const lowerRegex = /[a-z]/;
+    let newCodes = oldArr.map(char => {
+        if (upperRegex.test(char)) {
+            return (((charToInt(char) + shift - 65)) % 26) + 65
+        } else if (lowerRegex.test(char)) {
+            return (((charToInt(char) + shift - 97)) % 26) + 97
+        } else {
+            return charToInt(char)
+        }
+    }
+    );
     const newArr = newCodes.map(code => intToChar(code))
     const res = newArr.join("")
     return res;
-}
+};
