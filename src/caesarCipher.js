a4ffdbe1125c19a8b43a2fc0b3b7adbad808f114
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-24 17:25:13 +0100

    caesarCipher.js: Added s variable in order to handle negative shifts. Passes tests

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -9,14 +9,15 @@ function intToChar(code) {
 
 
 export function caesarCipher(str, shift) {
+    const s = shift > 0 ? shift : 26 + (shift % 26);
     const oldArr = str.split("")
     const upperRegex = /[A-Z]/;
     const lowerRegex = /[a-z]/;
     let newCodes = oldArr.map(char => {
         if (upperRegex.test(char)) {
-            return (((charToInt(char) + shift - 65)) % 26) + 65
+            return (((charToInt(char) + s - 65)) % 26) + 65
         } else if (lowerRegex.test(char)) {
-            return (((charToInt(char) + shift - 97)) % 26) + 97
+            return (((charToInt(char) + s - 97)) % 26) + 97
         } else {
             return charToInt(char)
         }
