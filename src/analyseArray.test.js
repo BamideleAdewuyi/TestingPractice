df10da5467f6759ca469a1a9dd8882f11f8d5257
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-25 22:12:19 +0100

    analyseArray.test.js: Wrote test for function returning an object with average property that has value of 4

--- src/analyseArray.test.js
+++ src/analyseArray.test.js
@@ -3,3 +3,7 @@ import { analyseArray } from "./analyseArray";
 test('Function returns something', () => {
     expect(analyseArray([3])).toBeDefined();
 });
+
+test('Function can calculate average', () => {
+    expect(analyseArray([1,8,3,4,2,6])['average']).toBe(4);
+});
