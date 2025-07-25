56b7397c33cea6f172fb753915aff0fa1c8828fe
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-25 22:38:00 +0100

    analyseArray.js: Function calculates average and minimum properties separately. Passes tests

--- src/analyseArray.js
+++ src/analyseArray.js
@@ -1,5 +1,8 @@
 export function analyseArray(arr) {
+    const average = arr.reduce(((acc, current) => (acc + current)))/arr.length;
+    const minimum = Math.min(...arr)
     return {
-        "average": 4
+        "average": average,
+        "min": minimum
     }
 };
