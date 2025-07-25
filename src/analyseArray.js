ceea94877ebb8f8c09895ed014cccffb03de5a51
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-07-25 22:44:23 +0100

    analyseArray.js: Function can calculate length. Passes tests

--- src/analyseArray.js
+++ src/analyseArray.js
@@ -2,9 +2,11 @@ export function analyseArray(arr) {
     const average = arr.reduce(((acc, current) => (acc + current)))/arr.length;
     const minimum = Math.min(...arr);
     const maximum = Math.max(...arr)
+    const length = arr.length;
     return {
         "average": average,
         "min": minimum,
-        "max": maximum
+        "max": maximum,
+        "length": length
     }
 };
